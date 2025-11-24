be95c5d1f49cddec3e5e1ae51c42bc71e0aefced
alicehan8 <alicehan@bu.edu>
2025-11-24 15:00:05 -0500

    Set up main activity and initialized sensors

--- app/src/main/java/com/example/assign6_1/MainActivity.kt
+++ app/src/main/java/com/example/assign6_1/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.assign6_1
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,13 +13,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.assign6_1.ui.theme.Assign6_1Theme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var pressureSensor: Sensor? = null
+
+    private var _pressure by mutableStateOf(1013.25f)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+
         enableEdgeToEdge()
         setContent {
             Assign6_1Theme {
@@ -28,7 +45,35 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        pressureSensor?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let{
+            _pressure = event.values[0]
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
 }
+
+@Composable
+fun AltimeterScreen(pressure: Float){
+
+}
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
